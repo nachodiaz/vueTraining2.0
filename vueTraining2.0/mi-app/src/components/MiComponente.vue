74c22c09e5bb6c14e0cb01f3389b424525c36d3a
Ignacio Díaz <nachodiaz@Ignacios-MacBook-Pro.local>
2020-03-24 23:55:37 -0300

    new change in gitIgnore

---
+++ vueTraining2.0/mi-app/src/components/MiComponente.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="MiComponente">
+        <h1>Mi Componente</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data(){
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style lang="">
+
+</style>
